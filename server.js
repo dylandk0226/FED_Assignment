56d9f7573a29e454c01de1f182dd87ca84bd6e91
dylandk0226 <kun_1993@hotmail.com>
2026-01-07 22:28:29 +0800

    Add direct to page dashboard.html

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.redirect('/dashboard.html');
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.static('Public'));
